ada573788fb73aec49bc795dce529354ecf0c0a6
ilija-dvp <ilija93eric@gmail.com>
2023-04-18 21:31:50 +0200

    test commit

---
+++ vjezbe.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// CTRL + SHIFT + I
+int main()
+{
+    int z;
+
+  cout << "Drugi test za Printanje!" << endl;
+cout << "Unesite broj: " << endl;
+cin >> z;
+cout <<z << endl;
+  return 0;
+}
